84b77091deba17478831b31508b4f3baad785973
kitchenevan <sirfcsn@gmail.com>
2018-02-09 14:53:23 +0900

    add search feat in contactMain.js
    using sort and filter method for making the feature.

--- src/components/contactMain.js
+++ src/components/contactMain.js
@@ -5,6 +5,7 @@ export default class ContactMain extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      keyword: '',
       contactData: [{
         name: 'Green',
         phone: '010-0000-0001'
@@ -19,10 +20,24 @@ export default class ContactMain extends React.Component {
         phone: '010-0000-0004'
       }]
     }
+    this.handleChange = this.handleChange.bind(this);
   };
+
+  handleChange(e) {
+    this.setState({
+      keyword: e.target.value
+    })
+  }
 
   render() {
     const mapToComponents = (data) => {
+      data.sort();
+      data = data.filter(
+        (contact) => {
+          return contact.name.toLowerCase()
+            .indexOf(this.state.keyword.toLowerCase()) > -1;
+        }
+      );
       return data.map((contact, i) => {
         return (<ContactInfo contact={contact} key={i}/>);
       });
@@ -31,6 +46,12 @@ export default class ContactMain extends React.Component {
     return(
       <div>
         <h1>Contacts</h1>
+        <input
+          name="keyword"
+          placeholder="Search"
+          value={this.state.keyword}
+          onChange={this.handleChange}
+        />
         <div>{mapToComponents(this.state.contactData)}</div>
       </div>
     );
